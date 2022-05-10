cbcd82c3e1394432e1e4f06449312e1221517668
Anto Subash <antosubash@live.com>
2022-05-10 13:49:14 +0200

    docker stack update

---
+++ common.ps1
@@ -0,0 +1,3 @@
+$rootPath = "C:\docker\data"
+$stackFolder = Join-Path $(pwd) "stacks"
+$stacks = @('portainer', 'mongo', 'postgres', 'redis', 'geoserver', 'keycloak', 'nuget', 'rabbitmq', 'seq')
--- init.ps1
+++ init.ps1
@@ -1,10 +1,19 @@
-Write-Output "Swarm Init"
-docker swarm init
-
-Write-Output "Creating volumes"
-$stacks = @('portainer','mysql','mongo','postgres-data','esdata', 'redis')
-
+. .\common.ps1
 foreach ($i in $stacks) {
-    Write-Output "Creating volume $i"
-    docker volume create $i
+    $folderPath = Join-Path $rootPath $i
+    if (!(Test-Path $folderPath)) {
+        Write-Output "Creating folder for $i"
+        New-Item -ItemType Directory -Force -Path $folderPath
+    }
+    Write-Output "Finding stack $i"
+    $isStackRunning = docker stack ls | Out-String -Stream | Select-String -Pattern $i
+    if ($isStackRunning) {
+        Write-Output "Stack $i is running"
+    } else {
+        Write-Output "Stack $i is not running"
+        Write-Output "Starting stack $i"
+        $stackFile = Join-Path $stackFolder "$i.yml"
+        Write-Output "Using stack file $stackFile"
+        docker stack deploy -c $stackFile $i
+    }
 }
